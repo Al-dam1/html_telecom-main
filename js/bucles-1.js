f3d935b7c098f0d7f21bf867bc1cd3c0d85fdb74
Damián Nicolás Alderete <damian.aldert@gmail.com>
2026-09-23 19:30:52 -0300

    ejercicios con  bucles

---
+++ js/bucles-1.js
@@ -0,0 +1,46 @@
+// Ejercicio 1
+
+// Usá while para mostrar los números del 1 al 10.
+// numero = 1
+// while (numero < 11){
+//     console.log('numero', numero);
+//     numero+=1;
+// }
+
+// Ejercicio 2
+
+// Usá while para mostrar los números del 10 al 1
+// let i = 10;
+// while (i > 0){
+//     console.log('numero ', i);
+//     i-=1;
+// }
+
+// Ejercicio 3
+
+// Usá while para mostrar solamente los números pares del 2 al 20.
+// let i = 1
+// while (i <= 20) {
+//     if (i % 2 == 0){
+//         console.log('numero par', i);
+        
+//     }
+//     i++;
+// }
+
+// Ejercicio 4
+
+// Usá un for para mostrar los números del 1 al 50.
+// for (let i = 1; i <=50; i++) {
+//     console.log(i);
+    
+// }
+
+// Usá un while para recorrer los números del 1 al 30.
+let i = 1;
+while (i <= 30) {
+    if (i % 2 == 1){
+        console.log('numero impar', i);
+    }
+    i++;
+}
